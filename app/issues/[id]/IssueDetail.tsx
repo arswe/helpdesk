6886e10402503cf4041b69c0d3f3d3171c33740a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-26 00:37:51 +0600

    Adding a Delete Button

--- app/issues/[id]/IssueDetail.tsx
+++ app/issues/[id]/IssueDetail.tsx
@@ -6,14 +6,16 @@ import ReactMarkdown from 'react-markdown'
 const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
     <>
-      <Heading as='h2'>{issue.title}</Heading>
+      <Heading as='h2' color='sky'>
+        {issue.title}
+      </Heading>
 
       <Flex className='space-x-3 ' my='2'>
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
 
-      <Card className='prose mt-4'>
+      <Card className='prose max-w-full ' mt='4'>
         <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </>
