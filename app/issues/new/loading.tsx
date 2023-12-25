9daaa5f85626ec101a8bbea6e79d5e1fe57ce5c8
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-25 17:24:09 +0600

    Additional Loading Skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,5 +1,16 @@
+import { Box } from '@radix-ui/themes'
+import delay from 'delay'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 const LoadingNewIssuePage = () => {
-  return <div>loading .....</div>
+  delay(3000)
+  return (
+    <Box className='max-w-xl'>
+      <Skeleton />
+      <Skeleton height='20rem' />
+    </Box>
+  )
 }
 
 export default LoadingNewIssuePage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,11 @@
 import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes'
-import delay from 'delay'
 import IssuStatusBadge from '../components/IssuStatusBadge'
 import Link from '../components/Link'
 import IssueActions from './IssueActions'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany()
-  await delay(1000)
 
   return (
     <div>
