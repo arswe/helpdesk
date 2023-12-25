1bb58287043727cd89824fc1132d9b27c714d630
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-25 18:44:56 +0600

    Understanding Caching

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,17 +8,15 @@ import { Issue } from '@prisma/client'
 import { Button, Callout, TextField } from '@radix-ui/themes'
 import axios from 'axios'
 import 'easymde/dist/easymde.min.css'
-import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
+import SimpleMDE from 'react-simplemde-editor'
 import { z } from 'zod'
-
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
 
 type IssueFormData = z.infer<typeof issueSchema>
 
-const IssueForm = ({ issue }: { issue: Issue }) => {
+const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter()
   const {
     register,
@@ -28,36 +26,34 @@ const IssueForm = ({ issue }: { issue: Issue }) => {
   } = useForm<IssueFormData>({
     resolver: zodResolver(issueSchema),
   })
-
   const [error, setError] = useState('')
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setSubmitting] = useState(false)
 
-  const onSubmit = handleSubmit(async (data: IssueFormData) => {
+  const onSubmit = handleSubmit(async (data) => {
     try {
-      setIsSubmitting(true)
-
-      if (issue) await axios.patch(`/api/issues/${issue.id}`, data)
+      setSubmitting(true)
+      if (issue) await axios.patch('/api/issues/' + issue.id, data)
       else await axios.post('/api/issues', data)
-      router.push('/issues')
+      router.push('/issues/list')
+      router.refresh()
     } catch (error) {
-      setIsSubmitting(false)
-      setError('An unexpected error occurred. Please try again or contact support if the problem persists.')
+      setSubmitting(false)
+      setError('An unexpected error occurred.')
     }
   })
 
   return (
-    <>
+    <div className='max-w-xl'>
       {error && (
         <Callout.Root color='red' className='mb-5'>
-          <Callout.Text> {error} </Callout.Text>
+          <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form className='max-w-xl space-y-3' onSubmit={onSubmit}>
+      <form className='space-y-3' onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input defaultValue={issue?.title} placeholder='Title' {...register('title')} />
         </TextField.Root>
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
-
         <Controller
           name='description'
           control={control}
@@ -65,13 +61,11 @@ const IssueForm = ({ issue }: { issue: Issue }) => {
           render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
         <Button disabled={isSubmitting}>
-          {issue ? 'Update Issue' : 'Submit New Issue'}
-          {isSubmitting && <Spinner />}
+          {issue ? 'Update Issue' : 'Submit New Issue'} {isSubmitting && <Spinner />}
         </Button>
       </form>
-    </>
+    </div>
   )
 }
 
