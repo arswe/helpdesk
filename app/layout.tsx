be8cffb578fda084459aae172d799fe5cb1ef80e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-25 13:05:01 +0600

    Setting Up Radix UI -

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
 import type { Metadata } from 'next'
 import { Inter, Rajdhani } from 'next/font/google'
 import NavBar from './NavBar'
@@ -19,8 +21,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={rajdhani.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   )
