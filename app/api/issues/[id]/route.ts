9f8484a753037cab96ff66c6def5d66c8f93988d
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-26 16:43:36 +0600

    Implementing the API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from '@/app/auth/authOptions'
-import { issueSchema } from '@/app/validationSchema'
+import { patchIssueSchema } from '@/app/validationSchema'
 import prisma from '@/prisma/client'
 import { getServerSession } from 'next-auth'
 import { NextRequest, NextResponse } from 'next/server'
@@ -10,8 +10,15 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
   const body = await request.json()
 
-  const validation = issueSchema.safeParse(body)
+  const validation = patchIssueSchema.safeParse(body)
   if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 })
+
+  const { assignedToUserId, title, description } = body
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({ where: { id: assignedToUserId } })
+    if (!user) return NextResponse.json({ error: 'Invalid user' }, { status: 400 })
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -21,7 +28,7 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: { title, description, assignedToUserId },
   })
 
   return NextResponse.json(updatedIssue, { status: 200 })
