8ad5538a89d9d69616b467281c8b194e69a3ff17
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-01 16:44:04 +0600

    short issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -20,8 +20,11 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const statuses = Object.values(Status)
 
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
-  const orderBy = searchParams.orderBy ? { [searchParams.orderBy]: 'asc' } : undefined
-  
+  const orderBy = columns
+  .map((column) => column.value)
+  .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined
 
   const issues = await prisma.issue.findMany({
     where: { status },
